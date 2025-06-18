19f4593eef2b54f03c2acdf372b128155d659761
Luis F Montoya <lfmontoya03@gmail.com>
2025-06-18 00:15:42 -0500

    imagenes menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,32 +1,36 @@
 // src/components/Menu/Menu.tsx
 import React from 'react';
 import './Menu.css'; // lo creamos en el siguiente paso
+import banquete from '../../assets/img/banquete.png';
+import elixir from '../../assets/img/elixir.jpg';
+import sinfonia from '../../assets/img/sinfonia.jpg';
+import nectar from '../../assets/img/nectar.jpg';
 
 const menuItems = [
   {
-    nombre: 'Ramen Miso',
+    nombre: 'Banquete diario',
     descripcion: 'Fideos artesanales en caldo miso con vegetales frescos.',
     precio: '$25.000',
-    imagen: '/img/banquete.png',
+    imagen: banquete,
   },
   {
-    nombre: 'Té Verde Japonés',
-    descripcion: 'Té tradicional servido caliente o frío.',
+    nombre: 'Elixir de Té Verde',
+    descripcion: 'Un delicado té de hierbas, de un verde esmeralda vibrante, en una taza singularmente pintada. El vapor, etéreo y danzarín, evoca la tranquilidad de un jardín escondido en el reino de los espíritus.',
     precio: '$8.000',
-    imagen: '/img/sinfoto.png',
+    imagen: elixir,
   },
   {
-    nombre: 'Dango',
-    descripcion: 'Brochetas dulces de arroz, postre típico japonés.',
+    nombre: 'Sinfonía de Vapor del Balneario',
+    descripcion: 'Una acogedora selección de tés y cafés, servidos en tazas artesanales.    El vapor se eleva como un suave susurro mágico, invitando a la calma en cualquier rincón de tu página.',
     precio: '$6.000',
-    imagen: '/img/elixir.png',
+    imagen: sinfonia,
   },
-    {
-        nombre: 'nectar',
-        descripcion: 'Néctar de frutas frescas, ideal para refrescarse.',
-        precio: '$5.000',
-        imagen: '/img/nectar.png',
-    }
+  {
+    nombre: 'Néctar Glacial del Río Dragón',
+    descripcion: 'Una bebida fría y refrescante, presentada en un recipiente místico que recuerda los tesoros del balneario. Sus colores vibrantes y el toque de fantasía capturan la esencia mágica de un verano',
+    precio: '$5.000',
+    imagen: nectar,
+  }
 ];
 
 export default function Menu() {
