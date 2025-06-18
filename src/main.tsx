3431db742ad5a97f68f66247af0860f3e391d903
sanbedoyau <sanbedoya@unal.edu.co>
2025-06-17 23:44:33 -0500

    ADD: `routes`
    Se añadieron rutas al proyecto para manejar las interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Navbar from './components/Navbar/Navbar.tsx';
-import { BathServices_Home } from './components/BathServices/BathService.tsx';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
-    <main className='main'>
-      <BathServices_Home/>
-    </main>
+    <App />
   </StrictMode>,
 )
