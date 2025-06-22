d22246256c3d915a34b8f065c5f2ade7a50707bd
sanbedoyau <sanbedoya@unal.edu.co>
2025-06-21 20:42:07 -0500

    REFACTOR: `ScrollToTop` => `useScrollToTop`
    Los nombres de los hooks deben comenzar con `use`. Además se estaba utilizando incorrectamente dentro de `App.tsx`. Se corrigió.
    También se añadió la ruta signup para el formulario de registro

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Menu from './pages/Menu';
 import NotFoundError from './pages/NotFoundError';
 import Jabones from './pages/Jabones';
 import Baños from './pages/Baños';
-import ScrollToTop from './components/hooks/scrollToTop';
+import useScrollToTop from './components/hooks/useScrollToTop';
+import SignUp from './pages/signUp';
 
 export default function App() {
+    useScrollToTop();
+
     return (
         <Router>
-            <ScrollToTop/>  
             <Routes>
                 <Route path='/' element={ <HomePage /> } />
                 <Route path='/menu' element={ <Menu /> } />
                 <Route path='/jabones' element={ <Jabones /> } />
                 <Route path='/baños' element={ <Baños /> } />
+                <Route path='/signup' element={ <SignUp /> } />
                 <Route path='*' element={ <NotFoundError /> } />
             </Routes>
         </Router>
